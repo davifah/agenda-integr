e8ed756699b914d5bd7fd2917c62685177b05d49
davifah <30874470+davifah@users.noreply.github.com>
2017-11-09 15:21:12 -0300

    servidor while read

--- serv.sh
+++ serv.sh
@@ -1,13 +1,30 @@
 a=0
+lala=0
 while :
 do
 	nc -l 2121 > tentesenha
 	a=`cat tentesenha`
-	b=`cat senhas | grep $a | wc -l`
-	if [ $b -eq 0 ];
+	while read lin
+	do
+		check=`echo $lin | grep $a | wc -l`
+		if [ check -eq 0 ]
+		then
+			continue
+		else
+			if [ $lin = $a ]
+			then
+				lala=1
+#				echo "1" > /dev/tcp/10.101.0.21/9999
+			else
+				continue
+			fi
+		fi
+	done > senhas
+#	b=`cat senhas | grep $a | wc -l`
+	if [ $lala -eq 0 ];
 	then
-		echo "0" > /dev/tcp/10.101.0.21/9999
+		echo "0" > /dev/tcp/10.101.0.22/9999
 	else
-		echo "1" > /dev/tcp/10.101.0.21/9999
+		echo "1" > /dev/tcp/10.101.0.22/9999
 	fi
 done
