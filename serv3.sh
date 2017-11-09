6dfdf3e0cb5c876d5054756830d7e06491d1fca3
AlexsanderJusto <31485761+AlexsanderJusto@users.noreply.github.com>
2017-11-09 15:55:23 -0300

    servidor com comparaçao certo lol

---
+++ serv3.sh
@@ -0,0 +1,29 @@
+check=0
+a=0
+lala=0
+while :
+do
+ 	nc -l 2121 > tentesenha2
+ 	a=`cat tentesenha2`
+	while read lin
+	do
+		check=`echo $lin | grep $a | wc -l`
+		if [ check -eq 0 ]
+		then
+			continue
+		else
+			if [ $lin == $a ]
+			then
+				lala=1
+			else
+				continue
+			fi
+		fi
+	done < senhas
+	if [ $lala -eq 0 ];
+ 	then
+		echo "0" > /dev/tcp/10.101.0.21/9999
+ 	else
+		echo "1" > /dev/tcp/10.101.0.21/9999
+ 	fi
+done
