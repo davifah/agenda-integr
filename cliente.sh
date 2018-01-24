2dda20df0987e4789519c47d326611a087d4a755
davifah <30874470+davifah@users.noreply.github.com>
2018-01-24 16:40:20 -0300

    To com odio do professor sou rebelde vtnc sem inutilidade

--- cliente.sh
+++ cliente.sh
@@ -1,6 +1,8 @@
 #Programa por davi e alexsander
 #Projeto Agenda para alunos
-ip1234=10.101.0.16
+ip1234=127.0.0.1
+portaserv=9999
+portaclien=9998
 clear
 dialog \
 	--title 'Agenda para Alunos' \
@@ -14,6 +16,19 @@ op=$(dialog \
 	1 'Administrador' \
 	2 'Usuário' \
 	3 'Sair' )
+	retval=$?
+	case $retval in
+		1)
+			clear
+			echo "Programa encerrado"
+			exit
+			;;
+		255)
+			clear
+			echo "Programa encerrado"
+			exit
+			;;
+	esac
 	case $op in
 		3)
 			clear
@@ -30,26 +45,43 @@ op=$(dialog \
 			        1 'Visualizar agenda' \
 			        2 'Sobre' \
 				3 'Sair' )
-			case $op in
+				retval=$?
+				case $retval in
+					1)
+						clear
+						echo "Programa encerrado"
+						exit
+						;;
+					255)
+						clear
+						echo "Programa encerrado"
+						exit
+						;;
+				esac
+				case $op in
 			        3)
 			                clear
 					echo "Programa encerrado"
 					break
 					;;
 				2)
-					echo 2: > /dev/tcp/$ip1234/9999
-					nc -l 9999 > sobre
-					nc -l 9999 > pic.jpg
+					echo 2: > /dev/tcp/$ip1234/$portaserv
+					(dialog  \
+						--title 'Aguarde'\
+						--infobox '\nFinalizando download...'\
+						0 0)
+					nc -l $portaclien > sobre
+					nc -lp $portaclien > pic.jpg
 					loc=`pwd`
-					(dialog \
-						--title 'SOBRE' \
-						--textbox $loc/sobre 0 0 )
+                                        (dialog \
+                                                --title 'SOBRE' \
+                                                --textbox $loc/sobre 0 0 )
 					display pic.jpg
 					;;
 				1)
 					clear
-					echo 1: > /dev/tcp/$ip1234/9999
-					nc -l 9999 > calen
+					echo 1: > /dev/tcp/$ip1234/$portaserv
+					nc -l $portaclien > calen
 					echo -e "   Código      Data               Informação"
 					while read lin
 					do
@@ -58,7 +90,7 @@ op=$(dialog \
 						inf=`echo $lin | cut -d"#" -f4`
 						echo -e "   "$cod"        "$data"             "$inf
 					done < calen
-					read -p "Press ENTER to continue..."
+					read -p "Pressione ENTER para continuar..."
 					;;
 			esac
 		done
@@ -73,15 +105,29 @@ op=$(dialog \
 			--stdout \
 			--passwordbox 'Digite sua senha' \
 			0 0 )
-		echo 0:$pswd > /dev/tcp/$ip1234/9999
-		nc -l 9999 > res
+		retval=$?
+		case $retval in
+			1)
+				clear
+				echo "Programa encerrado"
+				exit
+				;;
+			255)
+				clear
+				echo "Programa encerrado"
+				exit
+				;;
+		esac
+		echo 0:$pswd > /dev/tcp/$ip1234/$portaserv
+		nc -l $portaclien > res
 		a=`cat res`
 		if [ $a -eq 1 ];
 		then
 			dialog \
 				--title 'Administrador' \
-				--msgbox 'Logado com sucesso' \
+				--infobox '\nLogado com sucesso' \
 				0 0
+			sleep 0.5s
 			while :
 			do
 				op=$(dialog \
@@ -92,13 +138,41 @@ op=$(dialog \
 				        2 'Excluir item' \
 					3 'Adicionar novo item' \
 					4 'Modificar senha de líder' \
-				        5 'Sair' )
+				        5 'Sobre' \
+					6 'Sair' )
+				retval=$?
+				case $retval in
+					1)
+						clear
+						echo "Programa encerrado"
+						exit
+						;;
+					255)
+						clear
+						echo "Programa encerrado"
+						exit
+						;;
+				esac
 				case $op in
-				        5)
+				        6)
 				                clear
 						echo "Programa encerrado"
 						exit
 				                ;;
+					5)
+						echo 2: > /dev/tcp/$ip1234/$portaserv
+						(dialog  \
+							--title 'Aguarde'\
+							--infobox '\nFinalizando download...'\
+							0 0)
+						nc -l $portaclien > sobre
+						nc -lp $portaclien > pic.jpg
+						loc=`pwd`
+                	                        (dialog \
+                	                                --title 'SOBRE' \
+                	                                --textbox $loc/sobre 0 0 )
+						display pic.jpg
+						;;
 	       				4)
 						clear
 						echo "Modificar senha -- aguardar att"
@@ -113,8 +187,8 @@ op=$(dialog \
 				                ;;
 					1)
 						clear
-						echo 1: > /dev/tcp/$ip1234/9999
-						nc -l 9999 > calen
+						echo 1: > /dev/tcp/$ip1234/$portaserv
+						nc -l $portaclien > calen
 						echo -e "   Código      Data               Informação"
 						while read lin
 						do
--- serv.sh
+++ serv.sh
@@ -1,10 +1,14 @@
-ipagoranao=10.101.0.15
+#Programa por davi e alexsander
+#Projeto Agenda para alunos
+ipagoranao=127.0.0.1
+portaserv=9999
+portaclien=9998
 check=0
 a=0
 lala=0
 while :
 do
- 	nc -l 9999 > inf
+ 	nc -l $portaserv > inf
 	ch=`cat inf | cut -d":" -f1`
 	i=`cat inf | cut -d":" -f2`
 	case $ch in
@@ -26,17 +30,18 @@ do
 			done < senhas
 			if [ $lala -eq 0 ];
 		 	then
-				echo "0" > /dev/tcp/$ipagoranao/9999
+				echo "0" > /dev/tcp/$ipagoranao/$portaclien
 		 	else
-				echo "1" > /dev/tcp/$ipagoranao/9999
+				echo "1" > /dev/tcp/$ipagoranao/$portaclien
  			fi
 			;;
 		1)
-			cat calen > /dev/tcp/$ipagoranao/9999
+			cat calen > /dev/tcp/$ipagoranao/$portaclien
 			;;
 		2)
-			echo `cat sobre.txt` > /dev/tcp/$ipagoranao/9999
-			echo `cat fotop.jpg` > /dev/tcp/$ipagoranao/9999
+			nc -w 3 $ipagoranao $portaclien < sobre.txt
+#			echo `cat fotop.jpg` > /dev/tcp/$ipagoranao/$portaclien
+			nc -w 3 $ipagoranao $portaclien < fotop.jpg
 			;;
 
 	esac
